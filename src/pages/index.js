768d651e447ffc54adbff76964200fcb6ad5184f
Igor Souza <igorsouza.dev@gmail.com>
2020-04-11 17:24:15 -0300

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,24 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useEffect } from 'react';
+import { Link } from 'gatsby';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import api from '../services/api';
+const IndexPage = () => {
+  useEffect(() => {
+    async function getExchange() {
+      const response = await api.get('/latest?base=USD');
+      console.log(response.data);
+    }
+    getExchange();
+  }, []);
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hello world</h1>
+    </Layout>
+  );
+};
 
-export default IndexPage
+export default IndexPage;
